0564743022ab901f59a758e28c28bcafccf5dd80
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2012-07-23 23:10:35 -0300

    New Version

--- labmic/labmic.ino
+++ labmic/labmic.ino
@@ -3,28 +3,30 @@
 
 SoftwareSerial sim900(7, 8);
 int statusConfig;
-boolean configAPN();
+String host;
+String apn;
+boolean configAPN(String apn);
 void splitAndSend(String text);
-boolean verifyMessage(String text);
 void addSensor(String mote_id, String name, String value);
 void addMote(String rxPackets, String txPackets, String rxBytes, String txBytes);
-void initHttpRequest();
+void initHttpRequest(String ip);
 void finishHttpRequest();
 void postMethodHttp();
 boolean sendCommandAndWaitResp(String comando, String respostaEsperada, int tentativas, int delayEntreTentativas);
-void ShowSerialData();
 void debugMessage(String);
-
-int mote_id=1;
+String getSerialData();
+String getModemData();
 
 void setup()
 {
     sim900.begin(19200);    // the GPRS baud rate   
     Serial.begin(19200);    // the GPRS baud rate 
     statusConfig = 0;
+    host = "127.0.0.1";
+    apn = "tim";
 }
 
-boolean configAPN()
+boolean configAPN(String apn)
 {
     if(!sendCommandAndWaitResp("AT+CIPSHUT", "OK", 3, 500)) {
         return false;
@@ -37,83 +39,84 @@ boolean configAPN()
     if (!sendCommandAndWaitResp("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"", "OK", 3, 2000)) { //setting the SAPBR, the connection type is using gprs
         return false;
     }
-    if (!sendCommandAndWaitResp("AT+CSTT=\"tim.br\", \"tim\", \"tim\"", "OK", 3, 1000)) {
+    if (!sendCommandAndWaitResp("AT+CSTT=\"" + apn +".br\", \"" + apn + "\", \"" + apn +"\"", "OK", 3, 1000)) {
         return false;
     }
     if (!sendCommandAndWaitResp("AT+SAPBR=1,1", "OK", 2, 1000)) { //setting the SAPBR, for detail you can refer to the AT command manual
         return false;
     } else {
 
-    statusConfig = 1;
-    return true;
+        statusConfig = 1;
+        return true;
     }
 }
 
 void loop()
 {
     if (statusConfig == 0) {
-        configAPN();
+        configAPN(apn);
     }
-    String input;
-    if (Serial.available()) {
-        while(Serial.available() != 0) {
-            input.concat((char)Serial.read());
-        }
-    }
-
-    if (input.length() >= 0) {
-        splitAndSend(input);
+    String data = String(getSerialData());
+    if (data.length() > 0) {
+        splitAndSend(data);
     }
     delay(100);
-
-    ShowSerialData();
 }
-
-
-// Recebe uma String "::key;value::"
-// Separa em "key" "value"
 void splitAndSend(String text)
 {
-    String str;
-    String strOne;
-    String strTwo;
-    String strOneT;
-    String strTwoT;
-    int i = text.indexOf(';');
-    strOne = text.substring(0,i);
-    Serial.println(strOne);
-    if(!verifyMessage(strOne))
-        return;
-    int indexOfDouble = strOne.indexOf(':');
-    strOneT = strOne.substring(indexOfDouble+2, strOne.length());
-
-    strTwo = text.substring(i+1,text.length());
-    Serial.println(strTwo);
-    if(!verifyMessage(strTwo)) {
-        return;
+    String id = getId(text);
+    String key = getKey(text);
+    String value = getValue(text);
+    if ((id.length() > 0) && (key.length() > 0) && (value.length() > 0)) {
+        addSensor(id, key, value);
     }
-    indexOfDouble = strTwo.indexOf(':');
-    strTwoT = strTwo.substring(0,strTwo.length()-3);
-
-    addSensor((String)mote_id, strOneT, strTwoT);
 }
 
-boolean verifyMessage(String text)
+String getId(String text) {
+    int a, b;
+    String c = "{";
+    String d = "}";
+    String id = cutString(text, a, b);
+    return id;
+}
+
+String getKey(String text) {
+    int a, b;
+    String c = "<\\";
+    String d = "\\,";
+    
+    a = text.indexOf(c);
+    b = text.indexOf(d);
+    String key = cutString(text, a, b);
+    return key;
+}
+
+String getValue(String text) {
+    int a, b;
+    String c = ",\\";
+    String d = "\\>";
+    a = text.indexOf(c);
+    b = text.indexOf(d);
+    String value = cutString(text, a, b);
+    return value;
+}
+
+String cutString(String s, int inicio, int fim)
 {
-    String message(text);
-    int indexOfFirstDoubleDot = message.indexOf(':');
-    if(message.charAt(indexOfFirstDoubleDot) == ':' && message.charAt(indexOfFirstDoubleDot+1) == ':')
-        return true;
-
-    return false;
-
+    String str = "";
+    while(inicio < fim) {
+        str.concat((char) s.charAt(inicio++));
+    }
+    if (str > 0) {
+        return str;
+    }
 }
 
 void addSensor(String mote_id, String name, String value)
 {
-    initHttpRequest();
+    initHttpRequest(host);
 
-    String dataPost = "_method=POST&data[Sensor][name]=" + name + "&data[Sensor][value]=" + value + "&data[Sensor][mote_id]=" + mote_id;
+    String dataPost = "data[Sensor][name]=" + name + "&data[Sensor][value]=" + value + "&data[Sensor][mote_id]=" + mote_id;
 
     String httpData = "AT+HTTPDATA=";
     String delayToSend = ",3000";
@@ -126,7 +129,7 @@ void addSensor(String mote_id, String name, String value)
 
 void addMote(int id, String rxPackets, String txPackets, String rxBytes, String txBytes)
 {
-    initHttpRequest();
+    initHttpRequest(host);
     String httpData = "AT+HTTPDATA=";
     String delayToSend = ",3000";
 
@@ -138,28 +141,22 @@ void addMote(int id, String rxPackets, String txPackets, String rxBytes, String 
     delay(3000);
 
     finishHttpRequest();
-    mote_id++;
 }
 
-void initHttpRequest()
+void initHttpRequest(String ip)
 {
     sim900.println("AT+CSQ");
     delay(100);
 
-    ShowSerialData();// this code is to show the data from gprs shield, in order to easily see the process of how the gprs shield submit a http request, and the following is for this purpose too.
-
     sendCommandAndWaitResp("AT+HTTPINIT", "OK", 3, 2000); //init the HTTP request
-    ShowSerialData();
+    sendCommandAndWaitResp("AT+HTTPPARA=\"URL\",\"" + ip + "\"", "OK", 3, 2000);// setting the httppara, the second parameter is the website you want to access
 
-    sendCommandAndWaitResp("AT+HTTPPARA=\"URL\",\"150.162.10.20:8080\"", "OK", 3, 2000);// setting the httppara, the second parameter is the website you want to access
-    ShowSerialData();
 }
 
 void postMethodHttp()
 {
     // TODO: Verificar o status
     sendCommandAndWaitResp("AT+HTTPACTION=1", "OK", 3, 8000);// POST session start
-    ShowSerialData();
 }
 
 void finishHttpRequest()
@@ -173,33 +170,45 @@ boolean sendCommandAndWaitResp(String comando, String respostaEsperada, int tent
 
         sim900.println(comando);
         delay(delayEntreTentativas);
-
-        resposta = "";
-        while(sim900.available()!=0) {
-            resposta.concat((char)sim900.read());
+        resposta = getModemData();
+        if (resposta.indexOf(respostaEsperada) >= 0) {
+            debugMessage("OK Comando: " + comando + " Resposta esperada: " + respostaEsperada);
+            return true;
         }
-
         tentativas--;
     }
+    debugMessage("ERRO Comando : " + comando + " Resposta esperada: " + respostaEsperada + "Resposta recebida: " + resposta);
     return false;
-}
-
-void ShowSerialData() {
-    String resposta;
-    while(sim900.available()!=0) {
-        resposta.concat((char)sim900.read());
-    }
-
-    if (resposta.indexOf("ERROR") >= 0) {
-        debugMessage("!!! CME ERROR: "+ resposta + "!!!");
-        statusConfig = 0;
-    }
-    if(resposta.length() > 0) {
-        Serial.println(resposta);
-    }
 }
 
 void debugMessage(String msg)
 {
     Serial.println("DEBUG: " + msg);
+}
+
+String getModemData() {
+    String resposta;
+    while(sim900.available()!=0) {
+        resposta.concat((char) sim900.read());
+    }
+
+    if (resposta.indexOf("CLOSED") >= 0) {
+        debugMessage("!!! SERVER DOWN !!!");
+        statusConfig = 0;
+    }
+
+    if(resposta.length() > 0) {
+        return resposta;
+    }
+}
+
+String getSerialData() {
+    String data;
+    while(Serial.available()!=0) {
+        data.concat((char) Serial.read());
+    }
+
+    if(data.length() > 0) {
+        return data;
+    }
 }
